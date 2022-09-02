95a0520dd045a1470a8de4cf2220c0b2051451ea
APotekhin1 <alex01210@gmail.com>
2022-09-02 10:54:03 +0300

    Решение задачи 2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
             "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur " +
             "sint occaecat cupidatat non proident, sunt in culpa qui officia" +
             " deserunt mollit anim id est laborum.";
+
     public static void main(String[] args) {
         WordsChecker wordsChecker = new WordsChecker(TEXT);
         System.out.println("Есть ли слово est? " +
